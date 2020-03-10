31e6ace9c04f64a87579a90a1a5c2c3c8919b5fd
Royce <royce.com@gmail.com>
2020-03-10 20:58:58 +0800

    Initial

---
+++ scripts/router.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+
+import time
+
+import config
+import web
+import logger
+
+# Globals
+logger = logger.instance
+
+
+def reset():
+    logger.info('Script started.')
+    config.init_web()
+    web.start()
+
+    driver = web.driver
+
+    driver.get(config.url)
+    driver.set_window_size(1680, 947)
+    driver.find_element(By.ID, "showlogininfo").click()
+    driver.find_element(By.ID, "txt_Username").send_keys(config.username)
+    driver.find_element(By.ID, "txt_Password").send_keys(config.password)
+    driver.find_element(By.ID, "loginbutton").click()
+    driver.find_element(By.ID, "name_addconfig").click()
+    driver.find_element(By.ID, "name_maintaininfo").click()
+    driver.switch_to.frame(0)
+    driver.find_element(By.ID, "btnsaveandreboot").click()
+    driver.switch_to.alert.accept()
+
+    wait_time = 75
+    logger.info('Request submitted, sleeping {} seconds'.format(wait_time))
+    time.sleep(wait_time)
+
+
+if __name__ == '__main__':
+    reset()
